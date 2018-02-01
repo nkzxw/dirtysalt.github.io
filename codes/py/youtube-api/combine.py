934f5da359ccae9a4675d0c6a53981f01b2c59c9
yanz <dirtysalt1987@gmail.com>
2018-02-01 15:48:07 +0800

    x

--- codes/py/youtube-api/combine.py
+++ codes/py/youtube-api/combine.py
@@ -6,7 +6,7 @@ import pandas as pd
 from pandas import ExcelWriter
 
 parts = ['recipe', 'makeup', 'hairstyle', 'nailart', 'lifehack', 'homedecor', 'diy']
-writer = ExcelWriter('keywords-agg.xlsx')
+writer = ExcelWriter('keywords-combined.xlsx')
 for p in parts:
     df = pd.read_csv(p + '.csv')
     df.to_excel(writer, p)
