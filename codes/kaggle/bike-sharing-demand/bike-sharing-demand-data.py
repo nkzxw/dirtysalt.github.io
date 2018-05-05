120cdcc5c935ad90cff11e2e4d1db4109544ca47
yanz <dirtysalt1987@gmail.com>
2018-05-05 21:19:27 +0800

    x

--- codes/kaggle/bike-sharing-demand/bike-sharing-demand-data.py
+++ codes/kaggle/bike-sharing-demand/bike-sharing-demand-data.py
@@ -17,7 +17,6 @@ def extend_fields(df):
     df['dt_month'] = dt2.apply(lambda x: x.month)
     df['dt_hour'] = dt2.apply(lambda x: x.hour)
     df['dt_year'] = dt2.apply(lambda x: x.year)
-    df['dt_day2'] = dt2.apply(lambda x: '{}-{}-{}'.format(x.year, x.month, x.day))
     x = df
     x['temp'] = np.round(x['temp']).astype(int)
     x['atemp'] = np.round(x['atemp']).astype(int)
