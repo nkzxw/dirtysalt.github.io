6649ae17ea59f252971951ebcc3d7aced56e3241
yanz <dirtysalt1987@gmail.com>
2018-06-10 14:14:34 +0800

    x

--- codes/project-euler/p1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding:utf-8
-# Copyright (C) dirlt
-
-xs = [3 * i for i in range(1, 1000 // 3 + 1)]
-ys = [5 * i for i in range(1, 1000 // 5 + 1)]
-zs = list(set([x for x in xs + ys if x < 1000]))
-# print(zs)
-print(sum(zs))
