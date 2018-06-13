64367e39c39e4148506e176df0ac03ffcebcee31
yanz <dirtysalt1987@gmail.com>
2018-06-13 11:34:35 +0800

    x

--- codes/geeksforgeeks/smallest-number-subset.py
+++ codes/geeksforgeeks/smallest-number-subset.py
@@ -3,22 +3,31 @@
 # Copyright (C) dirlt
 
 
+# def solve(xs):
+#     xs.sort()
+#     n = len(xs)
+#     max_value = n * xs[-1]
+#     dp = [0] * (max_value + 1)
+#     dp[0] = 1
+#     for x in xs:
+#         for i in range(max_value - x, -1, -1):
+#             if not dp[i]:
+#                 continue
+#             v = i + x
+#             dp[v] = 1
+#     for i in range(0, max_value + 1):
+#         if not dp[i]:
+#             return i
+#     return max_value + 1
+
 def solve(xs):
     xs.sort()
-    n = len(xs)
-    max_value = n * xs[-1]
-    dp = [0] * (max_value + 1)
-    dp[0] = 1
+    res = 1
     for x in xs:
-        for i in range(max_value - x, -1, -1):
-            if not dp[i]:
-                continue
-            v = i + x
-            dp[v] = 1
-    for i in range(0, max_value + 1):
-        if not dp[i]:
-            return i
-    return max_value + 1
+        if x > res:
+            break
+        res += x
+    return res
 
 
 t = int(input())
