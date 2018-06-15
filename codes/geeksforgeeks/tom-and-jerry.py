b5b79b67d9df018ef5473bdaa03704644ad31569
yanz <dirtysalt1987@gmail.com>
2018-06-15 14:22:52 +0800

    x

--- codes/geeksforgeeks/tom-and-jerry.py
+++ codes/geeksforgeeks/tom-and-jerry.py
@@ -18,13 +18,67 @@ def prime_build_table(n):
 def prime_upper_bound(n):
     return min(n - 1, round(n ** 0.5) + 2)
 
-table = prime_build_table(10 ** 6)
-primes = {x for x in range(2, len(table)) if table[x]}
+
+def factor_build_table(n):
+    table = []
+    for i in range(n + 1):
+        table.append([])
+    res = 0
+    n2 = prime_upper_bound(n)
+    for i in range(2, n2):
+        for j in range(i, n // i + 1):
+            v = i * j
+            table[v].append(i)
+            res += 1
+    return table
+
+
+N = 10 ** 6
+
+# factor_table = factor_build_table(N)
+
+# dp = [0] * (N + 1)
+# dp[1] = 0
+# for x in range(2, N + 1):
+#     if dp[x - 1] == 0:
+#         dp[x] = 1
+#         continue
+#     ok = 0
+#     for f in factor_table[x]:
+#         f2 = x // f
+#         if dp[x - f] == 0 or dp[x - f2] == 0:
+#             ok = 1
+#             break
+#     dp[x] = ok
+#
+#
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     print(dp[n])
+
+prime_table = prime_build_table(N)
+
 
 def solve(n):
     dp = [0] * (n + 1)
     dp[1] = 0
-
+    for x in range(2, n + 1):
+        if dp[x - 1] == 0:
+            dp[x] = 1
+            continue
+        if prime_table[x]:
+            continue
+        ok = 0
+        for f in range(2, prime_upper_bound(x)):
+            if x % f != 0:
+                continue
+            f2 = x // f
+            if dp[x - f] == 0 or dp[x - f2] == 0:
+                ok = 1
+                break
+        dp[x] = ok
+    return dp[n]
 
 
 t = int(input())
--- codes/hackerrank/euler_util.py
+++ codes/hackerrank/euler_util.py
@@ -12,13 +12,38 @@ def prime_build_table(n):
     for i in range(2, n2):
         if table[i] == 0:
             continue
-        for j in range(2, n // i + 1):
+        for j in range(i, n // i + 1):
             table[i * j] = 0
     return table
 
 
 def prime_upper_bound(n):
     return min(n - 1, round(n ** 0.5) + 2)
+
+
+def check_prime_table():
+    print('check prime table ...')
+    n = 10 ** 6
+    table = prime_build_table(n)
+    for x in range(2, len(table)):
+        exp = is_prime(x)
+        val = bool(table[x])
+        if exp != val:
+            print('failed at {}. (exp = {}, val = {})'.format(x, exp, val))
+            break
+    print('PASSED!!!')
+
+
+def factor_build_table(n):
+    table = []
+    for i in range(n + 1):
+        table.append([])
+    n2 = prime_upper_bound(n)
+    for i in range(2, n2):
+        for j in range(i, n // i + 1):
+            v = i * j
+            table[v].append(i)
+    return table
 
 
 def is_prime(n):
@@ -74,3 +99,7 @@ def miller_rabin(n, k=10):
         if not check(a, s, d, n):
             return False
     return True
+
+
+if __name__ == '__main__':
+    check_prime_table()
