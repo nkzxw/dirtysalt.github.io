c070b8c0228d9cc0dcc28ef243d777434cebae03
yanz <dirtysalt1987@gmail.com>
2018-06-26 18:28:43 +0800

    x

---
+++ codes/leetcode/poor-pigs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Copyright (C) dirlt
+
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
+        pigs = 0
+        rounds = minutesToTest / minutesToDie
+        while True:
+            if (rounds + 1) ** pigs >= buckets:
+                break
+            pigs += 1
+        return pigs
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.poorPigs(1000, 15, 60))
