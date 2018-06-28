5dd5d8dbec795644c6713138df066bf77173c84c
yanz <dirtysalt1987@gmail.com>
2018-06-28 15:06:26 +0800

    x

--- codes/leetcode/insert-interval.py
+++ codes/leetcode/insert-interval.py
@@ -3,10 +3,20 @@
 # Copyright (C) dirlt
 
 # Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+import functools
+
+
+def cmp(x, y):
+    if x < y: return -1
+    if x > y: return 1
+    return 0
+
 
 class Solution(object):
     def insert(self, intervals, newInterval):
@@ -16,17 +26,17 @@ class Solution(object):
         :rtype: List[Interval]
         """
         intervals.append(newInterval)
-        intervals.sort(lambda x, y: cmp(x.start, y.start) or cmp(x.end, y.end))
+        intervals.sort(key=functools.cmp_to_key(lambda x, y: cmp(x.start, y.start) or cmp(x.end, y.end)))
         # print_intervals(intervals)
         res = []
-        x, y = intervals[0].start, intervals[0].end
+        start, end = intervals[0].start, intervals[0].end
         for i in range(1, len(intervals)):
             a, b = intervals[i].start, intervals[i].end
-            assert(x <= a)
-            if a <= y:
-                y = max(y, b)
+            assert start <= a
+            if a <= end:
+                end = max(end, b)
             else:
-                res.append((x, y))
-                x, y = a, b
-        res.append((x, y))
-        return [Interval(x_y[0], x_y[1]) for x_y in res]
+                res.append((start, end))
+                start, end = a, b
+        res.append((start, end))
+        return [Interval(p[0], p[1]) for p in res]
--- codes/leetcode/merge-intervals.py
+++ codes/leetcode/merge-intervals.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # coding:utf-8
 # Copyright (C) dirlt
+import functools
+
+
+def cmp(x, y):
+    if x < y:
+        return -1
+    elif x > y:
+        return 1
+    return 0
+
 
 class Solution(object):
     def merge(self, intervals):
@@ -9,20 +19,21 @@ class Solution(object):
         :rtype: List[Interval]
         """
         if not intervals: return []
-        intervals.sort(lambda x, y: cmp(x.start, y.start) or cmp(x.end, y.end))
+        intervals.sort(key=functools.cmp_to_key(lambda x, y: cmp(x.start, y.start) or cmp(x.end, y.end)))
         # print_intervals(intervals)
         res = []
-        x, y = intervals[0].start, intervals[0].end
+        start, end = intervals[0].start, intervals[0].end
         for i in range(1, len(intervals)):
             a, b = intervals[i].start, intervals[i].end
-            assert(x <= a)
-            if a <= y:
-                y = max(y, b)
+            assert (start <= a)
+            if a <= end:
+                end = max(end, b)
             else:
-                res.append((x, y))
-                x, y = a, b
-        res.append((x, y))
-        return [Interval(x_y[0], x_y[1]) for x_y in res]
+                res.append((start, end))
+                start, end = a, b
+        res.append((start, end))
+        return [Interval(p[0], p[1]) for p in res]
+
 
 # Definition for an interval.
 class Interval(object):
@@ -30,13 +41,16 @@ class Interval(object):
         self.start = s
         self.end = e
 
+
 def list_to_intervals(xs):
     return [Interval(x[0], x[1]) for x in xs]
 
+
 def print_intervals(xs):
     print(', '.join(['[%d, %d]' % (x.start, x.end) for x in xs]))
-    
+
+
 if __name__ == '__main__':
     s = Solution()
-    print_intervals(s.merge(list_to_intervals([[1,3],[2,6],[8,10],[15,18]])))
-    print_intervals(s.merge(list_to_intervals([[1,4], [0, 4]])))
+    print_intervals(s.merge(list_to_intervals([[1, 3], [2, 6], [8, 10], [15, 18]])))
+    print_intervals(s.merge(list_to_intervals([[1, 4], [0, 4]])))
