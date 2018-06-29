10f4e9041405c1aa8e7fe1f2bbbb349bca148224
yanz <dirtysalt1987@gmail.com>
2018-06-29 16:10:25 +0800

    x

--- codes/leetcode/validate-binary-search-tree.py
+++ codes/leetcode/validate-binary-search-tree.py
@@ -15,6 +15,31 @@ MAX_VALUE = (1 << 31)
 
 
 class Solution:
+    # def _isValidBST(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: bool
+    #     """
+    #
+    #     def check(root):
+    #         if not root:
+    #             return True, MAX_VALUE, MIN_VALUE
+    #
+    #         ok0, min0, max0 = check(root.left)
+    #         ok1, min1, max1 = check(root.right)
+    #
+    #         min2 = min(min0, root.val, min1)
+    #         max2 = max(max1, root.val, max0)
+    #
+    #         if root.val <= max0 or root.val >= min1 or not ok0 or not ok1:
+    #             return False, min2, max2
+    #
+    #         return True, min2, max2
+    #
+    #     ok, _, _ = check(root)
+    #     return ok
+
+
     def isValidBST(self, root):
         """
         :type root: TreeNode
@@ -31,27 +56,3 @@ class Solution:
             return False
 
         return check(root, MIN_VALUE, MAX_VALUE)
-
-    def _isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-
-        def check(root):
-            if not root:
-                return True, MAX_VALUE, MIN_VALUE
-
-            ok0, min0, max0 = check(root.left)
-            ok1, min1, max1 = check(root.right)
-
-            min2 = min(min0, root.val, min1)
-            max2 = max(max1, root.val, max0)
-
-            if root.val <= max0 or root.val >= min1 or not ok0 or not ok1:
-                return False, min2, max2
-
-            return True, min2, max2
-
-        ok, _, _ = check(root)
-        return ok
