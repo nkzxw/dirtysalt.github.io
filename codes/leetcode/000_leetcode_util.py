60d29364eae9396d056e5be79dd2a27c91477f24
yanz <dirtysalt1987@gmail.com>
2018-07-11 16:06:12 +0800

    x

--- codes/leetcode/000_leetcode_util.py
+++ codes/leetcode/000_leetcode_util.py
@@ -9,40 +9,44 @@ class TreeNode:
         self.right = None
 
 
+def is_palindrome(x):
+    s = str(x)
+    return s == s[::-1]
+
+
+def bf_next_palindrome(x):
+    while True:
+        x += 1
+        if is_palindrome(x):
+            return x
+
+
 def next_palindrome(x):
-    def _next_palindrome(ds):
-        n = len(ds)
-        s, e = 0, n - 1
-        ok = False
-        while s <= e:
-            if ds[s] != ds[e]:
+    def _find(x):
+        while True:
+            if is_palindrome(x):
+                return x
+            ds = list(str(x))
+            n = len(ds)
+            s, e = 0, n - 1
+            carry = 0
+            while s <= e:
                 if ds[s] > ds[e]:
                     ds[e] = ds[s]
-                else:
-                    v = ds[s]
-                    ds[s] = ds[e] = v + 1
-                ok = True
-            s += 1
-            e -= 1
-        if ok: return ds
+                elif ds[s] < ds[e]:
+                    carry = ord('9') - ord(ds[e]) + 1
+                    carry *= (10 ** s)
+                    break
+                s += 1
+                e -= 1
+            x = int(''.join(ds))
+            x += carry
 
-        m = (n - 1) // 2
-        while m >= 0 and ds[m] == 9:
-            m -= 1
-        if m >= 0:
-            # m + 1 .. n - 1 - (m + 1)
-            for p in range(m + 1, n - 1 - m):
-                ds[p] = 0
-            value = ds[m]
-            ds[m] = value + 1
-            ds[n - 1 - m] = value + 1
-            return ds
-        ds = [1] + [0] * (n - 1) + [1]
-        return ds
-
-    x = [int(c) for c in str(x)]
-    x = _next_palindrome(x)
-    return int(''.join([str(c) for c in x]))
+    if is_palindrome(x):
+        x = _find(x + 1)
+    else:
+        x = _find(x)
+    return x
 
 
 def list_to_tree(xs):
@@ -65,3 +69,16 @@ def list_to_tree(xs):
                 stack.append(r)
             i += 1
     return root
+
+
+def check_next_palindrome():
+    N = 10000
+    for x in range(1, N):
+        a = next_palindrome(x)
+        b = bf_next_palindrome(x)
+        if a != b:
+            print('x = {}, a = {}, b = {}'.format(x, a, b))
+
+
+if __name__ == '__main__':
+    check_next_palindrome()
--- codes/leetcode/prime-palindrome.py
+++ codes/leetcode/prime-palindrome.py
@@ -2,40 +2,37 @@
 # coding:utf-8
 # Copyright (C) dirlt
 
+def is_palindrome(x):
+    s = str(x)
+    return s == s[::-1]
+
+
 def next_palindrome(x):
-    def _next_palindrome(ds):
-        n = len(ds)
-        s, e = 0, n - 1
-        ok = False
-        while s <= e:
-            if ds[s] != ds[e]:
+    def _find(x):
+        while True:
+            if is_palindrome(x):
+                return x
+            ds = list(str(x))
+            n = len(ds)
+            s, e = 0, n - 1
+            carry = 0
+            while s <= e:
                 if ds[s] > ds[e]:
                     ds[e] = ds[s]
-                else:
-                    v = ds[s]
-                    ds[s] = ds[e] = v + 1
-                ok = True
-            s += 1
-            e -= 1
-        if ok: return ds
+                elif ds[s] < ds[e]:
+                    carry = ord('9') - ord(ds[e]) + 1
+                    carry *= (10 ** s)
+                    break
+                s += 1
+                e -= 1
+            x = int(''.join(ds))
+            x += carry
 
-        m = (n - 1) // 2
-        while m >= 0 and ds[m] == 9:
-            m -= 1
-        if m >= 0:
-            # m + 1 .. n - 1 - (m + 1)
-            for p in range(m + 1, n - 1 - m):
-                ds[p] = 0
-            value = ds[m]
-            ds[m] = value + 1
-            ds[n - 1 - m] = value + 1
-            return ds
-        ds = [1] + [0] * (n - 1) + [1]
-        return ds
-
-    x = [int(c) for c in str(x)]
-    x = _next_palindrome(x)
-    return int(''.join([str(c) for c in x]))
+    if is_palindrome(x):
+        x = _find(x + 1)
+    else:
+        x = _find(x)
+    return x
 
 
 def prime_upper_bound(n):
@@ -77,3 +74,4 @@ if __name__ == '__main__':
     print(s.primePalindrome(9999))
     print(next_palindrome(85709140))
     print(s.primePalindrome(85709140))
+    print(s.primePalindrome(10 ** 8))
