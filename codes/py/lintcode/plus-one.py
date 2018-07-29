27b2637d0b00fa88d9b6fc5926010609246a7043
yanz <dirtysalt1987@gmail.com>
2018-07-29 13:37:39 +0800

    x

---
+++ codes/py/lintcode/plus-one.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    @param digits: a number represented as an array of digits
+    @return: the result
+    """
+    def plusOne(self, digits):
+        # write your code here
+        n = len(digits)
+        carry = 1
+        for i in range(n-1, -1, -1):
+            carry += digits[i]
+            digits[i] = carry % 10
+            carry //= 10
+        if carry:
+            digits.insert(0, carry)
+        return digits
