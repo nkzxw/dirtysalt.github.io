f81c30914c804e0915e3cbcfd5be079d8c310ffa
yanz <dirtysalt1987@gmail.com>
2018-07-29 13:10:30 +0800

    x

--- codes/geeksforgeeks/shortest-path-from-1-to-n.py
+++ codes/geeksforgeeks/shortest-path-from-1-to-n.py
@@ -3,6 +3,9 @@
 # Copyright (C) dirlt
 
 # dijkstra
+from queue import PriorityQueue
+
+
 def solve(n):
     INF = 1 << 31
     dist = [INF] * (n + 1)
@@ -33,6 +36,39 @@ def solve(n):
     return dist[n]
 
 
+class Vertex:
+    def __init__(self, node, cost):
+        self.node = node
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+
+def solve_dj(n):
+    INF = 1 << 31
+    dist = [INF] * (n + 1)
+    edges = PriorityQueue()
+    dist[1] = 0
+    v = Vertex(node=1, cost=0)
+    edges.put(v)
+    while not edges.empty():
+        v = edges.get()
+        next_node = v.node + 1
+        if next_node <= n and (dist[next_node] > (dist[v.node] + 1)):
+            dist[next_node] = dist[v.node] + 1
+            next_v = Vertex(node=next_node, cost=dist[next_node])
+            edges.put(next_v)
+
+        next_node = v.node * 3
+        if next_node <= n and (dist[next_node] > (dist[v.node] + 1)):
+            dist[next_node] = dist[v.node] + 1
+            next_v = Vertex(node=next_node, cost=dist[next_node])
+            edges.put(next_v)
+
+    return dist[n]
+
+
 # dp
 def solve_dp(n):
     INF = 1 << 31
@@ -50,13 +86,15 @@ def solve_dp(n):
     return dp[n]
 
 
-# for n in range(1, 1000 + 1):
-#     a = solve(n)
-#     b = solve_dp(n)
-#     if a != b:
-#         print('disagree with n = {}, ({}, {})'.format(n, a, b))
-
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    print(solve_dp(n))
+for n in range(1, 1000 + 1):
+    a = solve_dj(n)
+    b = solve_dp(n)
+    if a != b:
+        print('disagree with n = {}, ({}, {})'.format(n, a, b))
+    else:
+        print('OK with n = {}'.format(n))
+#
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     print(solve_dp(n))
