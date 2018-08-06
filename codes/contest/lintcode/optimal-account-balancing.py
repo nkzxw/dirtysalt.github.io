90f0db5bb5c7288b6f05d23ab90f1fc8cffdeced
yanz <dirtysalt1987@gmail.com>
2018-08-07 07:51:47 +0800

    x

--- codes/contest/lintcode/optimal-account-balancing.py
+++ codes/contest/lintcode/optimal-account-balancing.py
@@ -2,6 +2,9 @@
 # coding:utf-8
 # Copyright (C) dirlt
 
+
+# TODO(yan): TLE. but should be correct. C++ version?
+
 from collections import defaultdict
 
 
@@ -19,51 +22,40 @@ class Solution:
             nodes[u] -= w
             nodes[v] += w
 
-        p0 = []
-        p1 = []
-        for k, v in nodes.items():
-            if v > 0:
-                p0.append(v)
-            elif v < 0:
-                p1.append(-v)
+        debts = [x for x in nodes.values() if x != 0]
+        # print(debts)
+        n = len(debts)
+        if n == 0: return 0
+        dp = [-1] * (1 << n)
+        for i in range(1, len(dp)):
+            balance = 0
+            count = 0
+            for j in range(n):
+                if (1 << j) & i:
+                    balance += debts[j]
+                    count += 1
 
-        p0.sort()
-        p1.sort()
+            if balance == 0:
+                exchange = count - 1
+                for j in range(1, i):
+                    if (i & j == j) and dp[j] != -1 and dp[i - j] != -1:
+                        exchange = min(exchange, dp[j] + dp[i - j])
+                dp[i] = exchange
+        # print(dp)
+        return dp[-1]
 
-        # 优先匹配相同的
-        def compress(p0, p1):
-            n = len(p0)
-            x0, x1 = [], []
-            idx0, idx1 = 0, 0
-            res = 0
-            while idx0 < n and idx1 < n:
-                if p0[idx0] == p1[idx1]:
-                    res += 1
-                    idx0 += 1
-                    idx1 += 1
-                elif p0[idx0] > p1[idx1]:
-                    x1.append(p1[idx1])
-                    idx1 += 1
-                else:
-                    x0.append(p0[idx0])
-                    idx0 += 1
-            x0.extend(p0[idx0:])
-            x1.extend(p1[idx1:])
-            return res, x0, x1
 
-        # 然后使用贪心算法
-        res, p0, p1 = compress(p0, p1)
-        n = len(p0)
-        idx0, idx1 = 0, 0
-        while idx0 < n:
-            if p0[idx0] == p1[idx1]:
-                idx0 += 1
-                idx1 += 1
-            elif p0[idx0] > p1[idx1]:
-                p0[idx0] -= p1[idx1]
-                idx1 += 1
-            else:
-                p1[idx1] -= p0[idx0]
-                idx0 += 1
-            res += 1
-        return res
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.balanceGraph([[0, 1, 10], [2, 0, 5]]))
+    print(sol.balanceGraph([[0, 1, 10], [1, 0, 1], [1, 2, 5], [2, 0, 5]]))
+    print(sol.balanceGraph(
+        [[7, 9, 1], [9, 8, 59], [4, 0, 46], [7, 6, 92], [7, 6, 92], [2, 3, 93], [1, 3, 96], [6, 8, 70], [2, 4, 36],
+         [3, 1, 23], [8, 9, 42], [8, 7, 45], [2, 4, 24], [9, 8, 17], [5, 7, 89], [0, 2, 65], [1, 0, 91], [5, 6, 2],
+         [8, 9, 24], [4, 1, 41]]))
+    print(sol.balanceGraph(
+        [[7, 6, 1], [4, 6, 59], [8, 9, 46], [7, 5, 92], [14, 13, 92], [2, 1, 93], [9, 8, 19], [14, 13, 72], [9, 8, 68],
+         [12, 16, 4], [14, 15, 74], [1, 3, 54], [3, 0, 63], [5, 7, 24], [5, 6, 17], [12, 14, 89], [8, 10, 65],
+         [2, 1, 91], [6, 5, 94], [1, 3, 85], [8, 10, 77], [15, 16, 40], [11, 9, 39], [10, 9, 42], [7, 6, 5],
+         [9, 10, 74], [9, 8, 73], [9, 8, 87], [9, 8, 56], [12, 16, 32], [2, 1, 25], [10, 11, 92], [14, 15, 84],
+         [5, 6, 22], [2, 1, 69], [3, 2, 56], [11, 8, 38], [3, 1, 3], [11, 8, 75], [0, 1, 49]]))
