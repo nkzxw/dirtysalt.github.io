d04431772492f8db64f8f29f3ff43e1e85e29e96
yanz <dirtysalt1987@gmail.com>
2018-08-06 23:01:58 +0800

    x

--- codes/contest/lintcode/minimum-height-trees.py
+++ codes/contest/lintcode/minimum-height-trees.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding:utf-8
 # Copyright (C) dirlt
-from queue import Queue
-
-
-# TODO(yan): C++ version?
 
 class Solution:
     """
@@ -16,34 +12,34 @@ class Solution:
     def findMinHeightTrees(self, n, edges):
         # Wirte your code here
 
-        adj = []
-        for i in range(n):
-            adj.append([])
+        if n == 1: return [0]
+        adj = [[] for x in range(n)]
+        deg = [0] * n
         for (u, v) in edges:
             adj[u].append(v)
             adj[v].append(u)
+            deg[u] += 1
+            deg[v] += 1
 
-        def bfs(x, minh):
-            queue = Queue()
-            visited = [0] * n
-            queue.put((x, 1))
-            visited[x] = 1
-            while not queue.empty():
-                v, d = queue.get()
-                if d > minh:
-                    break
+        leaves = [v for v in range(n) if deg[v] == 1]
+        depths = [0] * n
+        to_visits = []
+        for leaf in leaves:
+            to_visits.append((leaf, 1))
+            depths[leaf] = 1
+
+        nodes = n
+        while nodes > 2:
+            next_visits = []
+            for (v, d) in to_visits:
+                d2 = d + 1
                 for v2 in adj[v]:
-                    if visited[v2]:
-                        continue
-                    queue.put((v2, d + 1))
-                    visited[v2] = d + 1
-            return max(visited)
+                    deg[v2] -= 1
+                    if deg[v2] in (0, 1) and not depths[v2]:
+                        depths[v2] = d2
+                        next_visits.append((v2, d2))
+            nodes -= len(to_visits)
+            to_visits = next_visits
 
-        out = []
-        minh = n
-        for x in range(n):
-            h = bfs(x, minh)
-            out.append((h, x))
-            minh = min(minh, h)
-        res = [x[1] for x in out if x[0] == minh]
+        res = [x[0] for x in to_visits]
         return res
