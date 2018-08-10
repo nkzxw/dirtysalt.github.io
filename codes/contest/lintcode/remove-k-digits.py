996fef6dcd7869bd99946caa9cd0dbe874042c63
yanz <dirtysalt1987@gmail.com>
2018-08-10 19:40:09 +0800

    x

--- codes/contest/lintcode/remove-k-digits.py
+++ codes/contest/lintcode/remove-k-digits.py
@@ -2,42 +2,44 @@
 # coding:utf-8
 # Copyright (C) dirlt
 
-# class Solution:
-#     """
-#     @param num: a string
-#     @param k: an integer
-#     @return: return a string
-#     """
-#
-#     def removeKdigits(self, num, k):
-#         # write your code here
-#
-#         dp = []
-#         dp.append(['' for _ in range(k + 1)])
-#         dp.append(['' for _ in range(k + 1)])
-#
-#         now = 0
-#         for i in range(len(num)):
-#             print(i)
-#             ch = num[i]
-#             dp[1 - now][0] = dp[now][0] + ch
-#             for j in range(1, k + 1):
-#                 dp[1 - now][j] = min(dp[now][j - 1], dp[now][j] + ch)
-#             now = 1 - now
-#
-#         s = dp[now][k]
-#         idx = 0
-#         while idx < len(s) and s[idx] == '0':
-#             idx += 1
-#         if idx == len(s): return '0'
-#         return s[idx:]
+class Solution:
+    """
+    @param num: a string
+    @param k: an integer
+    @return: return a string
+    """
 
+    def removeKdigits(self, num, k):
+        # write your code here
 
-if __name__ == '__main__':
-    # with open('/Users/dirlt/Downloads/101.in') as fh:
-    #     num = eval(fh.readline())
-    #     k = eval(fh.readline())
-    #     sol = Solution()
-    #     print(sol.removeKdigits(num, k))
-    sol = Solution()
-    print(sol.removeKdigits('1432219', k=3))
+        if k == 0: return num
+        cuts = 0
+        saved = []
+        idx = 0
+
+        while idx < len(num):
+            v = num[idx]
+            if not saved:
+                saved.append(v)
+                idx += 1
+                continue
+            while saved and v < saved[-1]:
+                saved.pop()
+                cuts += 1
+                if cuts == k:
+                    break
+            if cuts == k:
+                break
+            saved.append(v)
+            idx += 1
+
+        res = ''.join(saved) + num[idx:]
+        if cuts < k:
+            res = res[:-(k - cuts)]
+        idx = 0
+        while idx < len(res) and res[idx] == '0':
+            idx += 1
+        res = res[idx:]
+        if res == '':
+            res = '0'
+        return res
