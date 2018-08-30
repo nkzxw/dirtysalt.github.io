85636613f4da9ab3e15d9a0b2aaab6967c882ad2
yanz <dirtysalt1987@gmail.com>
2018-08-30 18:00:36 +0800

    codes

---
+++ codes/contest/leetcode/beautiful-arrangement-ii.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Copyright (C) dirlt
+
+class Solution:
+    def constructArray(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+
+        ans = []
+        x = 1
+        d = 0
+        step = k
+        ans.append(x)
+        while step >= 1:
+            if d == 0:
+                x = x + step
+            else:
+                x = x - step
+            ans.append(x)
+            step -= 1
+            d = 1 - d
+
+        for x in range(2 + k, n + 1):
+            ans.append(x)
+        return ans
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.constructArray(10, 4))
+    print(sol.constructArray(10, 5))
+
